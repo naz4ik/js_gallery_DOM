e53a7f07755105213fb3ab7e8a3de2db066ecff9
Nazar Shtefaniuk <nazik200934@gmail.com>
2024-12-04 07:19:19 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const largeImage = document.getElementById('largeImg');
+const thumbnails = document.querySelectorAll('.gallery__img');
+
+thumbnails.forEach((thumbnail) => {
+  thumbnail.addEventListener('click', () => {
+    event.preventDefault(); // Запобігаємо стандартній дії посилання
+
+    const newSrc = thumbnail.src; // Отримуємо шлях до картинки
+
+    // Змінюємо джерело великої картинки
+    largeImage.src = newSrc;
+  });
+});

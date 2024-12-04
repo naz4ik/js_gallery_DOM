7c069f53d34b0cc7f541ad78a52bb1a04e64d771
Nazar Shtefaniuk <nazik200934@gmail.com>
2024-12-04 07:34:18 +0100

    make build

---
+++ index.5c6d8878.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.querySelector(".gallery").addEventListener("click",function(){event.preventDefault();var t=event.target;if("IMG"===t.tagName){var r=t.closest("a");r&&(e.src=r.href)}});
+//# sourceMappingURL=index.5c6d8878.js.map
